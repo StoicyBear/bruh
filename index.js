c4ad18547c89a0d19339b0319f87c4277119a4bd
StoicyBear <55302130+StoicyBear@users.noreply.github.com>
2019-09-13 19:49:15 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const token = 'NjIxODYxODg2Nzc3ODg0Njc3.XXrp_g.E0pI2Suyy_jB7W1JrLdBAogQH9I'
+
+bot.on ('ready', () =>{
+    console.log('this bot is online')
+})
+
+bot.on('message', msg=>{
+    if(msg.content ===  "?bruh"){
+        msg.reply('bruh moment');
+    }
+})
+
+bot.on('message', msg=>{
+    if(msg.content ===  "?bmoment"){
+        msg.reply('https://giphy.com/gifs/VIOkcgpsnA2Zy')
+    }
+
+})
+
+bot.on('message', message => {
+    if(message.content ===  ("?ping")) {
+            message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");        
+    }
+})
+
+bot.login(token);
